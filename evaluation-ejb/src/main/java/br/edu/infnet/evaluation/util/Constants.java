40e7f5f888ef190fa1a6f4c53a28f286260dd69a
Renan T. Pinzon <rpinzon@gmail.com>
2013-07-28 22:42:15 -0300

    constants

---
+++ evaluation-ejb/src/main/java/br/edu/infnet/evaluation/util/Constants.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2013 Renan T. Pinzon <rpinzon@gmail.com>
+ * 
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+package br.edu.infnet.evaluation.util;
+
+/**
+ * Class that contains all constants used into the system.
+ * 
+ * @author Renan T. Pinzon
+ */
+public final class Constants {
+
+	/**
+	 * To prevent this class from being instantiated.
+	 */
+	private Constants() {
+		// does nothing
+	}
+
+	public static final int MAX_ATTEMPTS_LOGIN = 5;
+
+}
